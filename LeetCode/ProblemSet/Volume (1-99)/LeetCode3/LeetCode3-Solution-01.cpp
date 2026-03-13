de4892fdfae481b84a82858087d544b7e2f2c508
Luminous <1094038421@qq.com>
2026-03-14 01:26:59 +0800

    LeetCode3

---
+++ LeetCode/ProblemSet/Volume (1-99)/LeetCode3/LeetCode3-Solution-01.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Luminous on 2026/3/14 1:00
+//
+
+
+#pragma GCC optimize(3)
+
+#include <bits/stdc++.h>
+
+#define endl "\n"
+
+
+void solve() {
+
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+
+    int T = 1;
+    //std::cin >> T;
+    while (T-- > 0) {
+        solve();
+    }
+
+    return 0;
+}
