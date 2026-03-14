b43e83215678f68c91788825a6a15a3afece8e11
Luminous <1094038421@qq.com>
2026-03-15 03:54:07 +0800

    LeetCode2904

---
+++ LeetCode/ProblemSet/Volume (2900-2999)/LeetCode2904/LeetCode2904-Solution-01.cpp
@@ -0,0 +1,45 @@
+//
+// Created by Luminous on 2026/3/15 3:37
+//
+
+
+#pragma GCC optimize(3)
+
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    std::string shortestBeautifulSubstring(std::string s, int k) {
+        const int n = (int) s.length();
+
+        std::string ans;
+        int cnt = 0;
+        for (int i = 0, j = 0; i < n; i++) {
+            if (s[i] == '1') {
+                cnt++;
+            }
+
+            if (cnt < k) {
+                continue;
+            }
+
+            while (cnt > k || s[j] == '0') {
+                if (s[j] == '1') {
+                    cnt--;
+                }
+                j++;
+            }
+
+            std::cout << k << " " << j << " " << i << std::endl;
+
+            int len = i - j + 1;
+            if (ans.empty() || len < ans.length()) {
+                ans = s.substr(j, len);
+            } else if (len == ans.length()) {
+                ans = std::min(ans, s.substr(j, len));
+            }
+        }
+
+        return ans;
+    }
+};
